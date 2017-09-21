59637abb9c2f1fea46fe4096b34281db4b588e1a
Zac Smith <zrs3141592@gmail.com>
2017-09-20 19:06:53 -0600

    Add background, scrolling

--- game.rb
+++ game.rb
@@ -4,11 +4,29 @@ class GameWindow <Gosu::Window
     super
     self.caption = 'Funky Burds'
 
-    @background = Gosu::Image.new(self, './images/background.png', false)
+    @scroll_x = 0
+    @background = Gosu::Image.new(self, 'images/background.png', false)
+    @foreground = Gosu::Image.new(self, 'images/foreground.png', true)
   end
   def button_down(button)
     close if button == Gosu::KbEscape
   end
+  def update
+    @scroll_x += 3
+    if @scroll_x > @foreground.width
+      @scroll_x = 0
+    end
+  end
+  def draw
+    @background.draw(0,0,0)
+    @foreground.draw(-@scroll_x,0,0)
+    @foreground.draw(-@scroll_x + @foreground .width,0,0)
+  end
 
 end
-window = GameWindow.new(800,600).show
+
+
+# Screen size
+width = 320
+height = 480
+window = GameWindow.new(width, height, false).show
